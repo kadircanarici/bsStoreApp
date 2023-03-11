bc4b33ff009b1af892df99a2c43c3c155f1f2b87
kadircanarici <kadircanarc@gmail.com>
2023-03-11 12:50:13 +0300

    Add project files.

---
+++ WebApi/Migrations/20230310202514_dbthreebook.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class dbthreebook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, 75m, "Yabancı" },
+                    { 2, 125m, "Beyaz Zambaklar Ülkesinde" },
+                    { 3, 300m, "Suç ve Ceza" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
---
+++ WebApi/Repositories/Config/BookConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Models;
+
+namespace WebApi.Repositories.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                new Book { Id = 1, Title = "Yabancı", Price = 75 },
+                new Book { Id = 2, Title = "Beyaz Zambaklar Ülkesinde", Price = 125 },
+                new Book { Id = 3, Title = "Suç ve Ceza", Price = 300 });
+        }
+    }
+}
