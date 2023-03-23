297fd2dd3f57f715add32daf01932181e47b6240
kadircanarici <kadircanarc@gmail.com>
2023-03-23 20:16:20 +0300

    options & head

--- Presentation/Controllers/BooksController.cs
+++ Presentation/Controllers/BooksController.cs
@@ -18,7 +18,7 @@ namespace Presentation.Controllers
         {
             _manager = manager;
         }
-
+        [HttpHead]
         [HttpGet]
         [ServiceFilter(typeof(ValidateMediaTypeAttribute))]
         public async Task<IActionResult> GetAllBooksAsync([FromQuery]BookParameters bookParameters)
@@ -94,6 +94,13 @@ namespace Presentation.Controllers
             await _manager.BookService.SaveChangesForPatchAsync(result.bookDtoForUpdate, result.book);
             
             return NoContent(); // 204
+        }
+
+        [HttpOptions]
+        public IActionResult GetBooksOptions()
+        {
+            Response.Headers.Add("Allow", "GET, PUT, POST, PATCH, DELETE, OPTIONS, HEAD");
+            return Ok();
         }
     }
 }
